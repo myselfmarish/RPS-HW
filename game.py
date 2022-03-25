0ee3f522dd90ff9c8af536c5c69ac604d2ed6f88
mashka kakashka <mashkakakashka@OleksandrSL.local>
2022-03-25 19:49:07 -0400

    Building out life logic in python

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 from random import randint
 
 choices = ["rock","paper", "scissors"]
+player_lives = 2
+computer_lives = 2
+total_lives = 2
+
 
 player_choice = input ("What do you want to choose? ROCK, PAPER OR SCISSORS? ")
 print( "user chose: " + player_choice)
@@ -14,17 +18,27 @@ if computer_choice == player_choice:
 elif computer_choice == "rock":
 	if player_choice == "scissors":
 		print("You lose!:(")
+		player_lives -= 1
 	else:
 		print("You win!:)")
+		computer_lives -= 1
 
 elif computer_choice == "paper":
 	if player_choice == "scissors":
 		print("You win!:)")
+		computer_lives -= 1
 	else:
 		print("You lose!:(")
+		player_lives -= 1
 
 elif computer_choice == "scissors":
 	if player_choice == "rock":
 		print("You win!:(")
+		computer_lives -= 1
 	else:
-		print("You lose!:)")
+		print("You lose!:(")
+		player_lives -= 1
+
+
+print("Player lives: ", player_lives)
+print("Computer lives: ", computer_lives)
